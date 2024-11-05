a2a1de226feaabebd388746d149c17dd9369180b
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-05 14:01:25 -0300

    lazy grid con scroll

--- app/src/main/java/com/example/gridcompose/MainActivity.kt
+++ app/src/main/java/com/example/gridcompose/MainActivity.kt
@@ -6,12 +6,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -31,22 +31,56 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MainScreen() {
 
-    val list = listOf("Text 1", "Text 2", "Text 3", "Text 4", "Text 5", "Text 6")
+    val list =
+        listOf(
+            "Text 1",
+            "Text 2",
+            "Text 3",
+            "Text 4",
+            "Text 5",
+            "Text 6",
+            "Text 7",
+            "Text 8",
+            "Text 9",
+            "Text 10",
+            "Text 11",
+            "Text 12",
+            "Text 13",
+            "Text 14",
+            "Text 15",
+            "Text 16",
+            "Text 17",
+            "Text 18",
+            "Text 19",
+            "Text 20"
+        )
 
     LazyVerticalGrid(
-        columns = GridCells.Fixed(4),
-        modifier = Modifier.fillMaxSize().padding(top = 106.dp)
+        columns = GridCells.Fixed(3),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 106.dp)
     ) {
-        items(list) {
-            Box(
-                modifier = Modifier.background(Color.Green)
-                    .size(100.dp)
-            ) {
-                Text(
-                    text = it,
-                    modifier = Modifier.padding(16.dp)
-                )
+
+        list.forEachIndexed { index, element ->
+            val span = if (index % 4 == 0) {
+                GridItemSpan(3)
+            } else {
+                GridItemSpan(1)
             }
+            item(span = { span }) {
+                Box(
+                    modifier = Modifier
+                        .background(Color.Green)
+                        .size(100.dp)
+                ) {
+                    Text(
+                        text = element,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+
         }
     }
 }
