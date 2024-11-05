a1906db8674e0f42ef6e2b867cb190517a577d2c
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-11-05 13:47:30 -0300

    creo grid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.gridcompose"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.gridcompose"
--- app/src/main/java/com/example/gridcompose/MainActivity.kt
+++ app/src/main/java/com/example/gridcompose/MainActivity.kt
@@ -1,48 +1,52 @@
 package com.example.gridcompose
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.gridcompose.ui.theme.GridComposeTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GridComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GridComposeTheme {
-        Greeting("Android")
+    val list = listOf("Text 1", "Text 2", "Text 3", "Text 4", "Text 5", "Text 6")
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(4),
+        modifier = Modifier.fillMaxSize().padding(top = 106.dp)
+    ) {
+        items(list) {
+            Box(
+                modifier = Modifier.background(Color.Green)
+                    .size(100.dp)
+            ) {
+                Text(
+                    text = it,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
     }
 }
